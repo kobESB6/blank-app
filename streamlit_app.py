bac2e4c1308cb11c991e6cacd287ff2f3fb3adbb
kobESB6 <146622909+kobESB6@users.noreply.github.com>
2024-11-12 09:25:59 +0000

    Coach Direct Access Profile Build

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("🎈ESB Profile")
+st.title("ESB Coaches Corner Profile")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    
+    "This is an example of a Direct Profile... Limited Access Let's start building this Profile!"
 )
+
+st.image("RenderedImage.jpg", caption="TD agatinst Wilson 1994" )
+st.text_input("Name")
+st.text_input("Date of Birth")
+st.text_input("height")
+st.text_input("weight")
+st.text_input("Name of School")
+st.text_input("GPA")
+st.text_input("Postiion")
+
+
+
