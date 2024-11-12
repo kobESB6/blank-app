96a41898382cfbc07057eef8c1c2e1a36b2f78d3
kobESB6 <146622909+kobESB6@users.noreply.github.com>
2024-11-12 09:38:18 +0000

    Camera FIleUpload feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,16 @@ import streamlit as st
 st.title("ESB Coaches Corner Profile")
 st.write(
     
-    "This is an example of a Direct Profile... Limited Access Let's start building this Profile!"
+    "This a Direct Access Profile... Limited Access Let's start building this Profile!"
 )
-
-st.image("RenderedImage.jpg", caption="TD agatinst Wilson 1994" )
 st.text_input("Name")
 st.text_input("Date of Birth")
-st.text_input("height")
-st.text_input("weight")
+st.text_input("Height")
+st.text_input("Weight")
 st.text_input("Name of School")
 st.text_input("GPA")
 st.text_input("Postiion")
-
-
+st.button("Submit")
+upload =st.file_uploader("Upload info about Student Athlete images,articles,")
+st.camera_input("Game Action Image")
 
